c76c2313f990e351fecf3cce3a42d9213f63e457
anajinguti <158512378+anajinguti@users.noreply.github.com>
2024-02-01 20:46:43 +0100

    Update 0. Cálculo de la norma de un vector con el módulo Numpy. 3 opciones..py

--- Tema 5/0. Cálculo de la norma de un vector con el módulo Numpy. 3 opciones..py
+++ Tema 5/0. Cálculo de la norma de un vector con el módulo Numpy. 3 opciones..py
@@ -3,7 +3,7 @@ import numpy as np
 import math
 v = np.array([1.0, 2.1, 3.4, 4.5, 5.6, 6.7, 7.8, 8.9, 10.0])
 sumatorio = 0.
-holaaaa
+
 #OPCIÓN 1. Aplicar la definición del sumatorio y hacer que pase por todos los valores del array
 for i in range (len(v)):
     print(i) #Se puede poner para comprobar el valor de i cada vez que se repite el bucle. 
